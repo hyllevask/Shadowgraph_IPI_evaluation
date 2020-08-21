575973fa13e85ffdfb42c94808bea29f60932377
Johan Öhman <jhn.ohman@gmail.com>
2020-08-21 10:37:01 +0200

    Now the code is commeted as well. Also removed some debugg prints.

--- droplet_shadow.py
+++ droplet_shadow.py
@@ -1,51 +1,64 @@
 #mainfile for running
+
+#Imort libreries
 import argparse
 import os
 import numpy as np
 
+
+#Setup the parser for commandline interface
 parser = argparse.ArgumentParser(description='Process the shadowgraph images.')
 parser.add_argument('--indir',type=str,default='data',help = 'Input Directory')
 parser.add_argument('--crop',type=tuple,default=(200,600,200,600),help="Crop Limits")
 parser.add_argument('--save_images',type=int, default=0,help = "Saves masks and histograms")
 args = parser.parse_args()
 
-def main():
+#Setup the main function
 
+def main():
+    #Pandas is used to store the data for each frame and
     import pandas as pd
-    df = pd.DataFrame(columns=['Particle','Area'])
-    index = 0
-    listan = []
+    listan = []     #The numpy arrays will be stored in this array
     if args.save_images == 1:
-        print("apa")
+        print("Image Save Enabled")
         if not os.path.exists("result_images"):
-            os.mkdir("result_images")
+            os.mkdir("result_images")       #Make result folder if it does not exsist
+    #Loop over the files in the dir
     for ii,filename in enumerate(os.listdir(args.indir)):
         print(ii)
         if filename.endswith('.bmp'):
-
+            #Call the main analyzing function
             area = analyze_image(filename,ii,args.save_images)
             listan.append(area)
         else:
             continue
+    #Create a dictionary from the list of results and make a dataframe from it
     df = pd.DataFrame({ i:pd.Series(value) for i, value in enumerate(listan) })
+    #Save the dataframe as csv
     df.to_csv('./df_list.csv')
 
 def analyze_image(filename,ii,save_images):
-    import skimage
+
     import matplotlib.pyplot as plt
     import skimage.measure
     import skimage.exposure
+
+    #read and crop the input
     im = plt.imread(args.indir +'/'+ filename)
     crop_limits = args.crop
     im = im[crop_limits[0]:crop_limits[1],crop_limits[2]:crop_limits[3]]
+
+    #Do the thresholding
     im_bw = im < 180
+    #Label and get regionprops
     all_labels = skimage.measure.label(im_bw)
     props = skimage.measure.regionprops(all_labels)
     area = []
+    #Extract the area for each particle
     for prop in props:
         area.append(prop.area)
     if save_images == 1:
-        print("hej")
+        #Generate and save images
         plt.figure(1,clear=True)
         plt.imshow(im < 180)
         plt.savefig('result_images/bw'+str(ii))
